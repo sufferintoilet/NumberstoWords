3f5d83d6179e21f76e0005755a7e1e2d7b52a8cd
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-11 02:31:13 +0900

    readnumber

---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        
+       Console.WriteLine("Enter number");
+       float number = float.Parse(Console.ReadLine());
+
+       int singlenumb = 3;
+        switch (singlenumb){
+            case 1:
+                Console.WriteLine("one");
+                break;
+            case 2:
+                Console.WriteLine("two");
+                break;
+            case 3:
+                Console.WriteLine("three");
+                break;
+            case 4:
+                Console.WriteLine("four");
+                break;
+            case 5:
+                Console.WriteLine("five");
+                break;
+            case 6:
+                Console.WriteLine("six");
+                break;
+            case 7:
+                Console.WriteLine("seven");
+                break;
+            case 8:
+                Console.WriteLine("eight");
+                break;
+            case 9:
+                Console.WriteLine("nine");
+                break;
+            default:
+                Console.WriteLine("out of ability");
+                break;}
+
+        
+        int doublenumb = 4;
+            switch (doublenumb){
+            case 0:
+                Console.WriteLine("ten");
+                break;
+            case 1:
+                Console.WriteLine("eleven");
+                break;
+            case 2:
+                Console.WriteLine("twelve");
+                break;
+            case 3:
+                Console.WriteLine("thirteen");
+                break;
+            case 4:
+                Console.WriteLine("fourtteen");
+                break;
+            case 5:
+                Console.WriteLine("fifteen");
+                break;
+            case 6:
+                Console.WriteLine("sixteen");
+                break;
+            case 7:
+                Console.WriteLine("seventeen");
+                break;
+            case 8:
+                Console.WriteLine("eighteen");
+                break;
+            case 9:
+                Console.WriteLine("nineteen");
+                break;
+            default:
+                Console.WriteLine("out of ability");
+                break;}
+            
+
+        int highdoublenumb = 8;
+            switch (highdoublenumb){
+            case 2:
+                Console.WriteLine("twenty");
+                break;
+            case 3:
+                Console.WriteLine("thirty");
+                break;
+            case 4:
+                Console.WriteLine("fourty");
+                break;
+            case 5:
+                Console.WriteLine("fifty");
+                break;
+            case 6:
+                Console.WriteLine("sixty");
+                break;
+            case 7:
+                Console.WriteLine("seventy");
+                break;
+            case 8:
+                Console.WriteLine("eighty");
+                break;
+            case 9:
+                Console.WriteLine("ninety");
+                break;
+            default:
+                Console.WriteLine("out of ability");
+                break;}
+
+
+        int tripplenumb = 6;
+            switch (tripplenumb){
+            case 1:
+                Console.WriteLine("one hundred");
+                break;
+            case 2:
+                Console.WriteLine("two hundred");
+                break;
+            case 3:
+                Console.WriteLine("three hundred");
+                break;
+            case 4:
+                Console.WriteLine("four hundred");
+                break;
+            case 5:
+                Console.WriteLine("five hundred");
+                break;
+            case 6:
+                Console.WriteLine("six hundred");
+                break;
+            case 7:
+                Console.WriteLine("seven hundred");
+                break;
+            case 8:
+                Console.WriteLine("eight hundred");
+                break;
+            case 9:
+                Console.WriteLine("nine hundred");
+                break;
+            default:
+                Console.WriteLine("out of ability");
+                break;}
+
+      
+        
+    }
+}
+
+
+            
+            
+            
+
+
+
+             
+
+    
